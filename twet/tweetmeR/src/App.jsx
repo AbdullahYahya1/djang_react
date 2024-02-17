29a262619360650ea7c23c92fbc015aa2dcef1ed
AbdullahYahya1 <al7rag50@gmail.com>
2024-02-17 13:20:34 +0300

    a lot

--- twet/tweetmeR/src/App.jsx
+++ twet/tweetmeR/src/App.jsx
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { TweetList } from "./tweets/index";
 import TweetForm from "./tweets/components";
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { TweetList } from "./components/TweetList";
+import TweetDetail from "./components/TweetDetail"; // Import the new component
 
-function App(props) {
-  const [csrftoken, setCsrftoken] = useState();
+function App() {
   return (
-    <>
-    <TweetList {...props}/>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={TweetList} />
+        <Route path="/tweet/:id" component={TweetDetail} /> {/* Add this route */}
+      </Switch>
+    </Router>
   );
 }
 
